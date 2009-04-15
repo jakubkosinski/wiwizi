4bcedf9e835deee91d96eada24c2716aadff2518
Jakub Kosinski <jakub.kosinski@ghandal.net>
2009-04-16 00:30:28 +0200

    big refactoring of searcher, added optparse, progressbar

--- searcher.rb
+++ searcher.rb
@@ -6,9 +6,10 @@
 
 require 'rubygems'
 require 'lib/wiwizi'
+require 'optparse'
+require 'progressbar'
 
-DATA_DIR_MASK = "./data/**/*"
-INDEX_TYPE    = :stemmer
+@options = {:type => :basic, :dir => "data"}
 
 def measure
   start = Time.now
@@ -17,30 +18,68 @@ def measure
   STDERR.flush
 end
 
-if $0 == __FILE__
-  begin
-    @index = Index.load_index()
-  rescue
-    measure do
-      @index = Index.new(INDEX_TYPE)
-      STDERR.print "Indexing..."
-      STDERR.flush
-      docs = Dir.glob(DATA_DIR_MASK).reject{|file| File.directory? file}
-      docs.each {|doc| @index.index_document(doc)}
+def prepare_index
+  return Index.load_index(@options[:index]) if @options[:index]
+  index = Index.new(@options[:type])
+  docs = Dir.glob(File.join(@options[:dir],"**","*")).reject{|file| File.directory? file}
+  pbar = ProgressBar.new("indexing", docs.size)
+  docs.each {|doc| index.index_document(doc); pbar.inc }
+  pbar.finish
+  if @options[:save]
+    STDERR.print "saving index..."
+    STDERR.flush
+    measure { index.save_index(@options[:save]) }
+  end
+  index
+end
+
+OptionParser.new do |opts|
+  opts.banner = "Boolan model search engine. (c) Jakub Kosinski, Jakub Kwasniewski"
+  opts.separator ""
+  opts.separator "Options:"
+
+  opts.on("-d","--data DIR", "path to data directory. Default is 'data'") do |dir|
+    @options[:dir] = dir
+  end
+  opts.on("-t","--type TYPE", [:basic, :stemmer], "index type, possible options: basic, stemmer. Default is basic") do |type|
+    @options[:type] = type
+  end
+  opts.on("-f","--file FILE", "read index from given file") do |file|
+    @options[:index] = file
+  end
+  opts.on("-b","--batch FILE", "read phrases from file (file format as described in task)") do |file|
+    buff = File.readlines(file)
+    @options[:times] = buff.first.to_i
+    @options[:phrases] = buff.slice(1..-1)
+  end
+  opts.on("-s","--save FILE", "saves index to file") do |file|
+    @options[:save] = file
+  end
+
+  opts.on_tail("-h","--help","shows this message") do
+    puts opts
+    exit
+  end
+end.parse(ARGV)
+
+index  = prepare_index
+search = Search.new(index)
+
+# batch mode
+if @options[:times] && @options[:phrases]
+  measure do
+    @options[:phrases].each do |phrase|
+      @options[:times].times { puts "Search results for phrase '#{phrase.strip}':\n#{search.search(phrase.strip).join("\n")}" }
+      puts
     end
   end
-  
-  if ARGV.first == "-i"
-    @search = Search.new(@index, INDEX_TYPE)
-    measure do
-      buff = File.readlines(ARGV[1])
-      times = buff.first.to_i
-      buff.slice(1..-1).each do |phrase|
-        times.times { puts "Search results for phrase '#{phrase.strip}':\n#{@search.search(phrase.strip).join("\n")}" }
-        puts
-      end
-    end
-  else
-    @index.save_to_file()
+# interactive mode
+else
+  puts "Entering interactive mode. Type in .q(uit) to exit"
+  loop do
+    print '> '
+    phrase = STDIN.readline.strip
+    break if ['.q','.quit'].include? phrase
+    measure{ puts search.search(phrase) }
   end
 end
