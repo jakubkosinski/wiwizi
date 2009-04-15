356d94c2acf52065f55406f4b16c05677a936342
Jakub Kosinski <jakub.kosinski@ghandal.net>
2009-04-16 00:28:28 +0200

    some search class refactoring, fixed problem with words from search phrase that doesn't exist in index

--- lib/search.rb
+++ lib/search.rb
@@ -5,25 +5,25 @@ require 'index'
 class Search
   attr_accessor :type
 
-  def initialize(index, type = :basic)
+  def initialize(index)
     @index = index
-    @type  = type
+    @type  = index.type
   end
 
   def basic_search(phrase)
     @sets = []
     @phrase_words = phrase.gsub(/[^\w\s]/,"").split
     @phrase_words.each do |word|
-      @sets << @index.index[word.downcase]
+      @sets << @index.index[word.downcase] || Set.new
     end
     generate_results
   end
 
   def stem_search(phrase)
     @sets = []
-    @phrase_words = phrase.gsub(/[^\w\s]/,"").split.collect{|i| i.stem}.delete_if {|i| Index::COMMON_WORDS.include? i.downcase}
+    @phrase_words = phrase.gsub(/[^\w\s]/,"").split.delete_if{|i| Index::COMMON_WORDS.include? i.downcase}.collect{|i| i.stem.downcase}
     @phrase_words.each do |word|
-      @sets << @index.index[word.downcase]
+      @sets << @index.index[word] || Set.new
     end
     generate_results
   end
@@ -39,14 +39,14 @@ class Search
 
   protected
   def generate_results
-    @results = @sets.inject{|r,s| r.intersection s}
+    @results = @sets.inject{|r,s| r & s}
     if @type == :basic
       @results.to_a
     else
       rank(@results.to_a)
     end
   end
-  
+
   def rank(results)
     results.sort { |x,y|
       @phrase_words.inject(0.0){|sum, i| sum += (@index.frequencies[i][x] + @index.positions[i][x])} <=>
